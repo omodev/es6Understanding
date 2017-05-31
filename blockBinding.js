70c4d1094faa6608b6f0688afb536826523bc48a
hassane omar <hassaneomar95@gmail.com>
2017-05-31 17:37:16 +0200

    block binding

---
+++ blockBinding.js
@@ -0,0 +1,15 @@
+function test(x) {
+
+					 if(x==5) {
+						    let val = 44 ;
+                console.log(val);
+								  return val ;
+					 }
+
+					 else{
+						    console.log(val);
+
+					 }
+		}
+
+		test(4);
